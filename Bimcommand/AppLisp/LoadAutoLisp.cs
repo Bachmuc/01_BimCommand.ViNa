b98d849c3be2d31be9b3b624eb6294e353ae5299
Bach Muc <ngyentanloc98@gmail.com>
2025-11-06 11:11:47 +0700

    Thêm chức năng mã máy và file HardwareInfo

--- Bimcommand/AppLisp/LoadAutoLisp.cs
+++ Bimcommand/AppLisp/LoadAutoLisp.cs
@@ -1,21 +1,24 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Runtime;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Exception = Autodesk.AutoCAD.Runtime.Exception;
 
 // using AutoCad 
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.Windows.Data;
-using Application = Autodesk.AutoCAD.ApplicationServices.Application;
-using Exception = Autodesk.AutoCAD.Runtime.Exception;
+
+using BimcommandCAD;
+using System.Windows.Forms; // Để sử dụng MessageBox
 
 namespace Bimcommand.AppLisp
 {
@@ -37,5 +40,20 @@ namespace Bimcommand.AppLisp
         {
         }
     }
+
+    public class KeyLincese
+    {
+        [CommandMethod("GETMYID")] // Tạo một lệnh tạm thời tên là GETMYID
+        public void ShowMyMachineId()
+        {
+            // --- ĐÂY LÀ PHẦN QUAN TRỌNG ---
+            // Gọi đến hàm static bạn vừa tạo trong class HardwareInfo
+            string machineId = HardwareInfo.GetCpuId();
+
+            // Hiển thị nó trong một hộp thoại
+            MessageBox.Show($"Mã máy của bạn là:\n\n{machineId}\n\nVui lòng sao chép và gửi mã này cho nhà cung cấp.", "Mã Kích Hoạt Phần Mềm");
+        }
+    }
+
 
 }
---
+++ Bimcommand/KeyLincese/HardwareInfo.cs
@@ -0,0 +1,29 @@
+﻿// File: HardwareInfo.cs
+using System.Management; // Đừng quên dòng này
+
+namespace BimcommandCAD
+{
+    public static class HardwareInfo
+    {
+        public static string GetCpuId()
+        {
+            try
+            {
+                string cpuInfo = string.Empty;
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT ProcessorId FROM Win64_Processor");
+                ManagementObjectCollection collection = searcher.Get();
+
+                foreach (ManagementObject obj in collection)
+                {
+                    cpuInfo = obj["ProcessorId"].ToString();
+                    break;
+                }
+                return cpuInfo;
+            }
+            catch
+            {
+                return "ErrorGettingCPU_ID";
+            }
+        }
+    }
+}
