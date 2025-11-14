2ad478bab9a29274bbabc66a17102b925a5850e0
Ngyễn Tấn Lộc <144118765+Bachmuc@users.noreply.github.com>
2025-11-15 00:43:20 +0700

    Updated Ver.1 về vector

---
+++ Bimcommand/Test/BaiToanVector.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using Autodesk.AutoCAD.ApplicationServices;
+//using Autodesk.AutoCAD.DatabaseServices;
+//using Autodesk.AutoCAD.EditorInput;
+//using Autodesk.AutoCAD.Geometry;
+//using Autodesk.AutoCAD.Runtime;
+//using Autodesk.AutoCAD.Colors;
+
+
+namespace Bimcommand.Test
+{
+    public class BaiToanVector
+    {
+        public const double EPSILON = 1E-06;
+        public class Vector2
+        {
+            //------------- Khai báo -------------//
+            private double X { get; set; }
+            private double Y { get; set; }
+
+            //Tạo ra vector có tọa độ (x,y)
+            public Vector2(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            //Tạo ra vector sao chép từ vector khác (clone Contructor)
+            //
+            // <Tạo bản sao độc lập>
+            // Vector v1 = new Vector(2,3);
+            // Vector v2 = new Vector(v1); //Tạo v2 giống hệt v1
+            // v2.X = 5; //Chỉ thay đổi v2, v1 không đổi
+            //
+            public Vector2(Vector2 vector)
+            {
+                this.X = vector.X;
+                this.Y = vector.Y;
+            }
+
+
+            //------------------- Phép toán-------------------//
+
+            //Nhân vector với một số
+            public static Vector2 operator *(Vector2 Vector, double Multiplier) //Vector * số
+            {
+                if(Vector == null)
+                {
+                    return null; //throw new ArgumentNullException("Vector không được null");
+                }
+
+                return new Vector2(Vector.X * Multiplier, Vector.Y * Multiplier);
+            }
+            public static Vector2 operator *(double Multiplier, Vector2 Vector) //số * Vector, đổi chỗ tham số
+            {
+                return Multiplier * Vector; ;
+            }
+
+            //Chiều dài vector
+            public double GetLength()
+            {
+                return Math.Sqrt(X * X + Y * Y);
+            }
+
+            //Tích vô hướng
+            public double Dot(Vector2 Vector)
+            {
+                return this.X * Vector.X + this.Y * Vector.Y;
+            }
+
+        }
+
+        public void Main()
+        {
+            double a = 0;
+            double b = 0;
+
+            Vector2 v1 = new Vector2(a, b);
+        }
+    }
+}
