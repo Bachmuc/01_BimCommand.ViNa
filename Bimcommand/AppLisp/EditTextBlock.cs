55c97196915cd87269a4668285d7d93eee0b23de
Bach Muc <ngyentanloc98@gmail.com>
2025-12-02 16:30:53 +0700

    Thêm Edit Text [E1]. Code cơ bản

---
+++ Bimcommand/AppLisp/EditTextBlock.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.Publishing;
+using Autodesk.AutoCAD.Windows.Data;
+
+namespace Bimcommand.AppLisp
+{
+    public class EditTextBlock
+    {
+        [CommandMethod("E2")]
+        public void EditText()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+
+            //Chọn đối tượng Text hoặc MText
+            PromptEntityOptions peo = new PromptEntityOptions("\nSelect Text or MText: ");
+            peo.SetRejectMessage("\nSelected object is not Text or MText.");
+            peo.AddAllowedClass(typeof(DBText), true);
+            peo.AddAllowedClass(typeof(MText), true);
+            peo.AddAllowedClass(typeof(AttributeReference), true);
+
+            PromptEntityResult per = ed.GetEntity(peo);
+            if (per.Status != PromptStatus.OK) return;
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                Entity ent = tr.GetObject(per.ObjectId, OpenMode.ForWrite) as Entity;
+                if(ent is DBText || ent is MText)
+                {
+                    // Prompt for new value, default to current
+                    PromptStringOptions pso = new PromptStringOptions("\nEnter new text content: ");
+                    pso.DefaultValue = currentText;
+                    pso.UseDefaultValue = true;
+                    pso.AllowSpaces = true;
+
+                    PromptResult pr = ed.GetString(pso);
+                    if (pr.Status == PromptStatus.OK)
+                    {
+                        if (ent is DBText dbText) dbText.TextString = pr.StringResult;
+                        else if (ent is MText mText) mText.Contents = pr.StringResult;
+                    }
+                }
+                else if(ent is MText BDText)
+                {
+                }
+                tr.Commit();
+            }
+
+        }
+    }
+}
